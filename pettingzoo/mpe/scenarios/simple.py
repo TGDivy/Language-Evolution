316a908f518074fec3d961ddb4beeb21efd5eb9f
tgdivy <tgdivy@gmail.com>
2021-10-20 20:00:48 +0100

    stable baselines 3 ppo

--- pettingzoo/mpe/scenarios/simple.py
+++ pettingzoo/mpe/scenarios/simple.py
@@ -10,13 +10,13 @@ class Scenario(BaseScenario):
         # add agents
         world.agents = [Agent() for i in range(1)]
         for i, agent in enumerate(world.agents):
-            agent.name = f'agent_{i}'
+            agent.name = f"agent_{i}"
             agent.collide = False
             agent.silent = True
         # add landmarks
         world.landmarks = [Landmark() for i in range(1)]
         for i, landmark in enumerate(world.landmarks):
-            landmark.name = 'landmark %d' % i
+            landmark.name = "landmark %d" % i
             landmark.collide = False
             landmark.movable = False
         return world
@@ -28,6 +28,7 @@ class Scenario(BaseScenario):
         # random properties for landmarks
         for i, landmark in enumerate(world.landmarks):
             landmark.color = np.array([0.75, 0.75, 0.75])
+            landmark.size = 0.15
         world.landmarks[0].color = np.array([0.75, 0.25, 0.25])
         # set random initial states
         for agent in world.agents:
@@ -35,7 +36,7 @@ class Scenario(BaseScenario):
             agent.state.p_vel = np.zeros(world.dim_p)
             agent.state.c = np.zeros(world.dim_c)
         for i, landmark in enumerate(world.landmarks):
-            landmark.state.p_pos = np_random.uniform(-1, +1, world.dim_p)
+            landmark.state.p_pos = np_random.uniform(-5, +5, world.dim_p)
             landmark.state.p_vel = np.zeros(world.dim_p)
 
     def reward(self, agent, world):
